f90497aeb634165864b4b708a88661699d26b52a
keem-hyun <hw813@naver.com>
2025-11-18 23:39:09 +0900

    add: color

--- presentation/src/main/java/com/someverse/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/someverse/presentation/ui/theme/Color.kt
@@ -7,9 +7,12 @@ val PrimaryPurple = Color(0xFF7250C9) // Main brand color
 
 // Accent colors
 val KakaoYellow = Color(0xFFFEE500) // Kakao login button color
+val SelectedRed = Color(0xFFFF6C6C) // Selected chip color
 
 // Neutral colors
 val White = Color.White
 val Black = Color(0xFF111111)
 
 val DescGray = Color(0xFF616772)
+val ChipGray = Color(0xFF9098A6) // Unselected chip text color
+val ChipBackgroundGray = Color(0xFFEBEFF5) // Unselected chip background
